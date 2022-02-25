c5e243bb726e38fd540ffecf3667c68d1f397bf5
Gracielle Gustavo <aulasnafatec@gmail.com>
2022-02-25 18:30:21 -0300

    segundo commit

---
+++ aula.js
@@ -0,0 +1,13 @@
+// começando...
+
+var linguagem = "JavaScript"
+console.log(`Aprendendo, ${linguagem}`)
+
+var nome = "Gustavo"
+const idade = 19
+nome = "Gracielle"
+
+if (idade >= 18) {
+    console.log("Parabéns,", nome, "\nVocê pode dirigir!")
+    console.log('b' + 'a' + +'a' + 'a')
+}
