80b0bc0c14633750b06faed187b1ebf17b5fce38
Gracielle Gustavo <aulasnafatec@gmail.com>
2022-02-25 19:06:36 -0300

    final da aula

--- aula.js
+++ aula.js
@@ -11,3 +11,12 @@ if (idade >= 18) {
     console.log("Parabéns,", nome, "\nVocê pode dirigir!")
     console.log('b' + 'a' + +'a' + 'a')
 }
+
+
+const nomes = ["ana paula", "rodrigo", "cleyfsson", "ana claudia", "amanda reis"]
+
+const apenasA = nomes.filter((listaDeNomes) => {
+    return listaDeNomes.startsWith("a")
+})
+
+console.log(apenasA)
