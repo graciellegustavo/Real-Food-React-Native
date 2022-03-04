90fdecb2d61b85e70a26112adeb5f3a15285beeb
Gustavo e Gracielle <aulasnafatec@gmail.com>
2022-03-04 18:07:28 -0300

    funcoes

--- aula.js
+++ aula.js
@@ -1,22 +1,37 @@
 // começando...
 
-var linguagem = "JavaScript"
-console.log(`Aprendendo, ${linguagem}`)
+// var linguagem = "JavaScript"
+// console.log(`Aprendendo, ${linguagem}`)
 
-var nome = "Gustavo"
-const idade = 19
-nome = "Gracielle"
+// var nome = "Gustavo"
+// const idade = 19
+// nome = "D.R.E"
 
-if (idade >= 18) {
-    console.log("Parabéns,", nome, "\nVocê pode dirigir!")
-    console.log('b' + 'a' + +'a' + 'a')
+// if (idade >= 18) {
+//     console.log("Parabéns,", nome, "\nVocê pode dirigir!")
+//     console.log('b' + 'a' + +'a' + 'a')
+// }
+
+
+// const nomes = ["ana paula", "rodrigo", "cleyfsson", "ana claudia", "amanda reis"]
+
+// const apenasA = nomes.filter((listaDeNomes) => {
+//     return listaDeNomes.startsWith("a")
+// })
+
+// hello = auu => "da" + auu 
+
+// console.log(hello("rarararara"))
+function f(funcao) {
+    funcao()
 }
 
+function g() {
+    function outraFuncao() {
+        console.log("produzida por g")
+    }
+    return outraFuncao()
+}
 
-const nomes = ["ana paula", "rodrigo", "cleyfsson", "ana claudia", "amanda reis"]
+f(g)
 
-const apenasA = nomes.filter((listaDeNomes) => {
-    return listaDeNomes.startsWith("a")
-})
-
-console.log(apenasA)
